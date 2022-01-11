8222e0fd57ce9b43b82b1eee870a40821902c2b8
dbarber1988 <95519768+dbarber1988@users.noreply.github.com>
2022-01-11 16:39:57 +0000

    Delete Importing_Data_CSV_to_DF.py

--- Importing_Data_CSV_to_DF.py
+++
@@ -1,6 +0,0 @@
-# Importing data from a CSV file saved locally on my laptop into Python as a Pandas DataFrame
-import pandas as pd
-df = pd.read_csv(r'C:\Users\Darren\Documents\Professional Development\Intro to data analytics UCD\Assignment\AnnualTicketSales.csv')
-print(df.head())
-
-
